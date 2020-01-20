f013e5392f49adeb24877b08a123831a1f5f42fb
Mufti05 <59869552+Mufti05@users.noreply.github.com>
2020-01-20 16:27:38 +0700

    Add files via upload

---
+++ MkbM.sh
@@ -0,0 +1,33 @@
+#!bin/sh
+clear
+toilet -f big -F gay Mufti
+echo "khusus Privat Agil(Mr.Tomen)"
+echo "Dibuat Oleh Admin Author Mufti(Mr.Killers)"
+sleep 3
+clear
+echo "Kita Bisa By Mufti"
+sleep 3
+read -p "masukkan No Hp Korban:" no
+clear
+echo "[1]Spam"
+echo "[2]Keluar"
+echo "Gel Agel Jok Mong omong neng Rek Arek"
+echo "Maki Dek sekolaan Jok Rame"
+read -p "Pilih No nya:" pl
+if [ $pl = 1 ]
+then
+    clear
+    toilet -f big -F gay Mufti 05
+    echo "Jok Lali Dungo"
+    echo "Jien Sho"
+    sleep 2
+    git clone https://github.com/ibnusyawall/spam-wa
+    cd spam-wa
+    npm i
+    node index.js
+fi
+if [ $pl = 2 ]
+then
+    echo "By by agel"
+    exit
+fi
